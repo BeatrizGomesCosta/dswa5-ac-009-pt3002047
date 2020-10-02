8f3b04a0689a85fe812b23482614b42676aedf0a
Beatriz Gomes Costa <beatrizgomesc@hotmail.com>
2020-10-02 13:05:56 -0300

    AC

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var app = require('./config/express')();
 
-const url = 'mongodb+srv://dswa5:dswa5@cluster0.flwr9.mongodb.net/ifsp?retryWrites=true&w=majority';
+const url = 'mongodb+srv://BeatrizGomesCosta:bgc201100@cluster0.gdojq.mongodb.net/test?authSource=admin&replicaSet=atlas-oqekkv-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
 require('./config/database.js')(url);
 
 
